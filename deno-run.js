bbfa7e4579864a5d6019b96f3dae28796fcbf24b
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-06-15 21:36:11 +0530

    Stores command output to work with using 'pipped'

--- deno-run.js
+++ deno-run.js
@@ -4,3 +4,12 @@ const greetCommand = new Deno.Command("echo", {
 });
 
 await greetCommand.output();
+
+
+const greet = new Deno.Command("echo", {
+  args:["Hello, world of scripting languages!"],
+  stdout:"piped"
+})
+
+const { stdout } = await greet.output();
+console.log("STDOUT: ", new TextDecoder().decode(stdout));
